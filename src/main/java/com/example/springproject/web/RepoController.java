3bf29cd72b853c66397f2cdc8ea94e63c19501d7
Joy-Mine <1525323329@qq.com>
2022-12-21 00:27:33 +0800

    Final Commit(Almost)

--- src/main/java/com/example/springproject/web/RepoController.java
+++ src/main/java/com/example/springproject/web/RepoController.java
@@ -61,10 +61,6 @@ public class RepoController {
         repoService=new RepoServiceImpl();
         return repoService.findInfo();
     }
-    @GetMapping("/getInfo4")
-    public String get_info3(){
-        return "666";
-    }
 
     @GetMapping("/getInfo2")
     public ArrayList<String> getInfo_2() throws IOException {
@@ -116,7 +112,7 @@ public class RepoController {
     }
 
     @GetMapping("/getInfo3")
-    public ArrayList<String>[] getInfo_3() throws SQLException, IOException, ParseException {
+    public ArrayList<String>[] getInfo_3() throws SQLException{
         ArrayList<String>[] arrayList=new ArrayList[10];
         arrayList[0]=new ArrayList<>();//Contributors Num;openIssue num;closedIssue num;Commits num;
                                         //均值，极值，方差
@@ -243,6 +239,152 @@ public class RepoController {
         statement.close();
         Druid.closeAll(connection);
         return arrayList;
+    }
+    @GetMapping("/getInfo4")
+    public ArrayList<String>[] get_info4() throws SQLException {
+        ArrayList<String>[] arrayList=new ArrayList[10];
+        arrayList[0]=new ArrayList<>();//Contributors Num;openIssue num;closedIssue num;Commits num;
+        //均值，极值，方差
+        arrayList[1]=new ArrayList<>();//Contributors Info
+        arrayList[2]=new ArrayList<>();//[2][0]-Releases-Num; Releases
+        arrayList[3]=new ArrayList<>();//Commits between Releases
+        arrayList[4]=new ArrayList<>();//Time distribution of the number of submissions
+
+        Connection connection = Druid.getConnection();
+        Statement statement=connection.createStatement();
+        String sql="select count(id) from contributor2;";
+        ResultSet result=statement.executeQuery(sql);
+        result.next();
+        arrayList[0].add(result.getString(1));
+
+        sql="select count(number) from openissue2;";
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[0].add(result.getString(1));
+
+        sql="select count(number) from closedissue2;";
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[0].add(result.getString(1));
+
+        sql="select count(message) from commits2;";
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[0].add(result.getString(1));
+
+        sql="select 24*sum(datediff(updated_at,created_at))/count(number) from closedissue2;";//平均值,单位：小时
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[0].add(result.getString(1));
+        sql="select max(ok)-min(ok) from (select datediff(updated_at,created_at)ok from closedissue2)nb;";//极值，单位：天
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[0].add(result.getString(1));
+        sql="select sum(pow((datediff(updated_at,created_at)-161.4),2))/count(number) from closedissue2;";//方差，单位：天
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[0].add(result.getString(1));
+
+        sql="select count(assets_url) from release2;";
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[2].add(result.getString(1));
+
+
+        sql="select * from contributor2 limit 10;";
+        result=statement.executeQuery(sql);
+//        while (result.next()){
+        for(int i=1;i<=10;++i){
+            result.next();
+            String id=result.getString(1);
+            String name=result.getString(2);
+            String contributions=result.getString(3);
+            String avatar_url=result.getString(4);
+            String html_url=result.getString(5);
+
+            arrayList[1].add(id);
+            arrayList[1].add(name);
+            arrayList[1].add(contributions);
+            arrayList[1].add(avatar_url);
+            arrayList[1].add(html_url);
+        }
+
+
+        sql="select * from release2 limit 2;";
+        result=statement.executeQuery(sql);
+//        while (result.next()){
+        for(int i=1;i<=2;++i){
+            result.next();
+            String assets_url=result.getString(1);
+            String created_at=result.getString(2);
+            String published_at=result.getString(3);
+            String name=result.getString(4);
+
+            arrayList[2].add(assets_url);//[2][] start form 1
+            arrayList[2].add(created_at);
+            arrayList[2].add(published_at);
+            arrayList[2].add(name);
+        }
+
+//        while (result.next()){
+        for(int i=2;i<=2;++i){
+            result.next();
+            String  upTime= arrayList[2].get(4*(i-2)+3);
+            String downTime=arrayList[2].get(4*(i-1)+3);
+
+            sql="select count(message) from commits2 where post_date " +
+                    "between str_to_date('"+downTime+"', '%Y-%m-%d %H') and str_to_date('"+upTime+"', '%Y-%m-%d %H');";
+
+            System.out.println(downTime);
+            System.out.println(upTime);
+            System.out.println(sql);
+
+            result=statement.executeQuery(sql);
+            result.next();
+            int count=result.getInt(1);
+            arrayList[3].add(String.valueOf(count));
+        }
+
+        sql="select count(message) from commits2 where post_date between str_to_date('2015-01-01','%Y-%m-%d %H') and str_to_date('2016-01-01','%Y-%m-%d %H');";
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[4].add(result.getString(1));
+        sql="select count(message) from commits2 where post_date between str_to_date('2016-01-01','%Y-%m-%d %H') and str_to_date('2017-01-01','%Y-%m-%d %H');";
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[4].add(result.getString(1));
+        sql="select count(message) from commits2 where post_date between str_to_date('2017-01-01','%Y-%m-%d %H') and str_to_date('2018-01-01','%Y-%m-%d %H');";
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[4].add(result.getString(1));
+        sql="select count(message) from commits2 where post_date between str_to_date('2018-01-01','%Y-%m-%d %H') and str_to_date('2019-01-01','%Y-%m-%d %H');";
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[4].add(result.getString(1));
+        sql="select count(message) from commits2 where post_date between str_to_date('2019-01-01','%Y-%m-%d %H') and str_to_date('2020-01-01','%Y-%m-%d %H');";
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[4].add(result.getString(1));
+        sql="select count(message) from commits2 where post_date between str_to_date('2020-01-01','%Y-%m-%d %H') and str_to_date('2021-01-01','%Y-%m-%d %H');";
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[4].add(result.getString(1));
+        sql="select count(message) from commits2 where post_date between str_to_date('2021-01-01','%Y-%m-%d %H') and str_to_date('2022-01-01','%Y-%m-%d %H');";
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[4].add(result.getString(1));
+        sql="select count(message) from commits2 where post_date between str_to_date('2022-01-01','%Y-%m-%d %H') and str_to_date('2023-01-01','%Y-%m-%d %H');";
+        result=statement.executeQuery(sql);
+        result.next();
+        arrayList[4].add(result.getString(1));
+
+
+
+        result.close();
+        statement.close();
+        Druid.closeAll(connection);
+        return arrayList;
+
     }
 
 }
